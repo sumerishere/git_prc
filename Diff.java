0b38bae83cfd53cf590fa4198e2cd5c8ff1fd3bf
Pathan Sumer Khan <ksumer597@gmail.com>
2023-10-12 03:23:13 +0530

    added some small changes

--- Diff.java
+++ Diff.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 class Diff{
     public static void main(String arg[]){
-        int a = 4;
-        int b = 8;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter int a :");
+        int a = sc.nextInt();
+        System.out.print("enter int b :");
+        int b = sc.nextInt();
         
-        int c = b-a;
-        System.out.println(c);
+        if (a > b){
+            int sub = a-b;
+            System.out.println("the substraction of a and b :"+sub);
+        }else{
+            int sub2 = b-a;
+            System.out.println("the substraction of a and b :"+sub2);
+        }
+        
+
     }
 }
